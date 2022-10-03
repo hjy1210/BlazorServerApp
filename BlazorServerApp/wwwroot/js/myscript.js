7a66ce7a9b4098c44eb3f542d5e766d96f8f93c7
HonJang Yang <hjyanghj@gmail.com>
2022-10-03 15:11:35 +0800

    插入影像檔較大時，會發生錯誤。

---
+++ BlazorServerApp/wwwroot/js/myscript.js
@@ -0,0 +1,50 @@
+﻿var ckeditors = {};
+var config = {
+    fontFamily: {
+        options: [
+            'default', 'Arial', 'sans - serif', 'Courier New', 'Courier', 'monospace', 'Courier',
+            '標楷體',
+            '細明體',
+            'Times New Roman'
+        ],
+        supportAllValues: true
+    },
+    alignment: {
+        options: ['left', 'right', 'center']
+    },
+    toolbar: {
+        items: ['heading', 'htmlEmbed', '|',
+            'bold', 'italic', 'underline', 'highlight', 'link', 'alignment', 'bulletedList', 'numberedList', '|',
+            'outdent', 'indent', '|',
+            'imageUpload', 'blockQuote', 'insertTable', 'codeBlock', 'findAndReplace', '|',
+            'fontBackgroundColor', 'fontColor', 'fontFamily', 'fontSize', '|',
+            'horizontalLine', 'specialCharacters', 'undo', 'redo', 'sourceEditing'
+        ]
+    },
+    language: 'en',
+    image: {
+        toolbar: [
+            'imageTextAlternative', 'imageStyle:inline', 'imageStyle:block', 'imageStyle:side', 'linkImage', 'toggleImageCaption'
+        ]
+    },
+    table: {
+        contentToolbar: [
+            'tableColumn', 'tableRow', 'mergeTableCells', 'tableCellProperties', 'tableProperties', 'toggleTableCaption'
+        ]
+    },
+    licenseKey: '',
+}
+
+window.createCKEditor = function (editorId) {
+    ClassicEditor
+        .create(document.querySelector('#' + editorId), config)
+        .then(newEditor => {
+            ckeditors[editorId] = newEditor;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+window.getDataFromEditor = function (editorId) {
+    return ckeditors[editorId].getData();
+}
